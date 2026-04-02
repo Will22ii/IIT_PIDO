532cc48b2a8af8ec9d107ff745d64a96a41b442d
Will2iz <dlgustkd980920@gmail.com>
2026-04-02 10:07:38 +0900

    FS update 4

--- pipeline/run_pipelines.py
+++ pipeline/run_pipelines.py
@@ -46,12 +46,12 @@ PROBLEM_SUITE: list[ProblemCase] = [
         n_samples=450,
         repeats=10,
     ),
-    ProblemCase(
-        problem_name="rosenbrock_nodummy",
-        known_optimum={"x1": 1.0, "x2": 1.0, "x3": 1.0, "x4": 1.0, "x5": 1.0},
-        n_samples=450,
-        repeats=10,
-    ),
+    # ProblemCase(
+    #     problem_name="rosenbrock_nodummy",
+    #     known_optimum={"x1": 1.0, "x2": 1.0, "x3": 1.0, "x4": 1.0, "x5": 1.0},
+    #     n_samples=450,
+    #     repeats=10,
+    # ),
     ProblemCase(
         problem_name="cantilever_beam",
         known_optimum={"H": 7.0, "h1": 0.1, "b1": 9.48482, "b2": 0.1},
@@ -59,24 +59,24 @@ PROBLEM_SUITE: list[ProblemCase] = [
         repeats=25,
 
     ),
-    ProblemCase(
-        problem_name="cantilever_beam_nodummy",
-        known_optimum={"H": 7.0, "h1": 0.1, "b1": 9.48482, "b2": 0.1},
-        n_samples=90,
-        repeats=25,
-    ),
+    # ProblemCase(
+    #     problem_name="cantilever_beam_nodummy",
+    #     known_optimum={"H": 7.0, "h1": 0.1, "b1": 9.48482, "b2": 0.1},
+    #     n_samples=90,
+    #     repeats=25,
+    # ),
     ProblemCase(
         problem_name="goldstein_price",
         known_optimum={"x1": 0.0, "x2": -1.0},
         n_samples=150,
         repeats=50,
     ),
-    ProblemCase(
-        problem_name="goldstein_price_nodummy",
-        known_optimum={"x1": 0.0, "x2": -1.0},
-        n_samples=150,
-        repeats=50,
-    ),
+    # ProblemCase(
+    #     problem_name="goldstein_price_nodummy",
+    #     known_optimum={"x1": 0.0, "x2": -1.0},
+    #     n_samples=150,
+    #     repeats=50,
+    # ),
     ProblemCase(
         problem_name="six_hump_camel",
         known_optimum=[
@@ -86,15 +86,15 @@ PROBLEM_SUITE: list[ProblemCase] = [
         n_samples=50,
         repeats=25,
     ),
-    ProblemCase(
-        problem_name="six_hump_camel_nodummy",
-        known_optimum=[
-            {"x1": 0.0898, "x2": -0.7126},
-            {"x1": -0.0898, "x2": 0.7126},
-        ],
-        n_samples=50,
-        repeats=25,
-    ),
+    # ProblemCase(
+    #     problem_name="six_hump_camel_nodummy",
+    #     known_optimum=[
+    #         {"x1": 0.0898, "x2": -0.7126},
+    #         {"x1": -0.0898, "x2": 0.7126},
+    #     ],
+    #     n_samples=50,
+    #     repeats=25,
+    # ),
 ]
 
 
